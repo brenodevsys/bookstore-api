3702b89c8442991d4fd4370e24d2c4083a46f7e6
Breno Delgado <brenodelgado.dev@gmail.com>
2021-11-17 12:11:53 -0300

    finalizando as classes de dominio

--- src/main/java/com/breno/bookstore/domain/Livro.java
+++ src/main/java/com/breno/bookstore/domain/Livro.java
@@ -1,14 +1,30 @@
 package com.breno.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import org.hibernate.annotations.ManyToAny;
+
+public class Livro implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String titulo;
 	private String nome_autor;
 	private String texto;
-
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
